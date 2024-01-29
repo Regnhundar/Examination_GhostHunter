5a7f17a16a0840e36bb5787c6fa8b8132a438610
Magnus Nilsson <regnhundar@gmail.com>
2024-01-29 11:31:09 +0100

    lade till dev branch, lade till 3st users i vår users array och lade till vår första funktions branch

--- scripts/users.js
+++ scripts/users.js
@@ -1,5 +1,16 @@
 /* Här skapar ni en array bestående av minst 3 st användarobjekt */
 
 let users = [
-    
+    {
+     username: asd,
+     password: asd
+    },
+    {
+     username: qwe,
+     password: qwe
+    },
+    {
+     username: zxc,
+     password: zxc
+    }
 ];
