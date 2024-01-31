2c158de2728c7611dde305819586b3cdb24b63b5
Jacob Collen <jcbcollen@icloud.com>
2024-01-31 15:04:36 +0100

    function gaOver fort. endel buggar att fixa

--- scripts/script.js
+++ scripts/script.js
@@ -41,20 +41,22 @@ function validateLogin () {
     } catch (error) {
         error.nodeRef.focus();
         document.querySelector(`#msg`).textContent = error.msg
-        console.log(error);
+
     }
 }
 
 function initiateGame () {
 
     let ghostContainer = document.createElement(`div`);
+
     ghostContainer.classList.add(`ghost-container`);
+
 
     let mainRef = document.querySelector(`main`);
     mainRef.appendChild(ghostContainer);
+    ghostContainer.innerHTML= ``;
     
     oGameData.ghostsToCatch = Math.floor(Math.random() * 6) + 10;
-
 
     for (let i = 0; i < oGameData.ghostsToCatch; i++){
 
@@ -89,10 +91,39 @@ function pointGoesDown () {
     event.target.classList.toggle(`d-none`);
     event.target.nextElementSibling.classList.toggle(`d-none`)
     oGameData.ghostsToCatch--;
+    gameOver();
 }
 
 function pointGoesUp () {
     event.target.classList.toggle(`d-none`);
     event.target.previousElementSibling.classList.toggle(`d-none`)
     oGameData.ghostsToCatch++;
+}
+
+function gameOver () {
+
+    if (oGameData.ghostsToCatch === 0) {
+
+        let ghostContainer = document.querySelector(`.ghost-container`);
+        
+        ghostContainer.innerHTML= ``;
+        let playAgainContainer = document.createElement(`div`);
+        playAgainContainer.classList.add(`play-again-container`);
+
+        let winnerText = document.createElement(`h2`);
+        winnerText.textContent = `Grattis, du klarade det!`;
+
+        let playAgainButton = document.createElement(`button`);
+        playAgainButton.textContent = `Spela igen`;
+        playAgainButton.addEventListener(`click`, () => {
+            document.querySelector(`.play-again-container`).classList.add(`d-none`);
+            initiateGame();
+        });
+
+        ghostContainer.appendChild(playAgainContainer);
+        playAgainContainer.appendChild(winnerText);
+        playAgainContainer.appendChild(playAgainButton);
+
+    }
+    
 }
